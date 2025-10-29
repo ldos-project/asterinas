7c1e414b44c2324d1b2636d757dff2fb686a5d87
Aneesh Durg <aneeshdurg17@gmail.com>
2025-10-29 17:18:33 -0500

    benchmarking script

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -77,6 +77,7 @@ pub(crate) mod vdso;
 pub mod vm;
 
 mod benchmark_consts;
+pub mod benchmarks;
 
 #[ostd::main]
 #[controlled]
@@ -132,152 +133,6 @@ fn ap_init() {
         .spawn();
 }
 
-#[allow(dead_code)]
-mod test_bench_overhead {
-    use super::*;
-    trait MyTrait: Any {
-        fn do_work(&mut self);
-    }
-
-    struct MyStruct {
-        calls: usize,
-    }
-
-    impl MyTrait for MyStruct {
-        #[inline(never)]
-        fn do_work(&mut self) {
-            self.calls += 1;
-        }
-    }
-
-    struct MyStruct2 {
-        calls: usize,
-    }
-
-    impl MyTrait for MyStruct2 {
-        #[inline(never)]
-        fn do_work(&mut self) {
-            self.calls += 2;
-        }
-    }
-
-    #[inline(never)]
-    fn bench_call_overhead(s: &mut Box<MyStruct>) {
-        let now = time::clocks::RealTimeClock::get().read_time();
-        for _ in 0..1000000 {
-            s.do_work();
-        }
-        let end = time::clocks::RealTimeClock::get().read_time();
-        println!("1M direct calls in {:?}", end - now);
-    }
-
-    #[inline(never)]
-    fn bench_dyn_call_overhead(s: &mut Box<dyn MyTrait>) {
-        let now = time::clocks::RealTimeClock::get().read_time();
-        for _ in 0..1000000 {
-            s.do_work();
-        }
-        let end = time::clocks::RealTimeClock::get().read_time();
-        println!("1M dyn calls in in {:?}", end - now);
-    }
-}
-
-#[allow(dead_code)]
-fn produce_bench(
-    q: &Arc<dyn OQueue<u64>>,
-    completed: &Arc<AtomicUsize>,
-    completed_wq: &Arc<ostd::sync::WaitQueue>,
-) {
-    println!("Starting producers");
-    // Start all producers
-    for tid in 0..benchmark_consts::N_THREADS {
-        let mut cpu_set = ostd::cpu::set::CpuSet::new_empty();
-        cpu_set.add(ostd::cpu::CpuId::try_from(tid + 1).unwrap());
-        ThreadOptions::new({
-            let completed = completed.clone();
-            let completed_wq = completed_wq.clone();
-            let producer = q.attach_producer().unwrap();
-            // let producer = q.clone(); //.attach_producer().unwrap();
-            move || {
-                let now = time::clocks::RealTimeClock::get().read_time();
-                for _ in 0..benchmark_consts::N_MESSAGES_PER_THREAD {
-                    producer.produce(0);
-                }
-                let end = time::clocks::RealTimeClock::get().read_time();
-                println!(
-                    "[producer-{}-{:?}] sent msg in {:?}",
-                    tid,
-                    ostd::cpu::CpuId::current_racy(),
-                    end - now
-                );
-                completed.fetch_add(1, Ordering::Relaxed);
-                completed_wq.wake_all();
-            }
-        })
-        .cpu_affinity(cpu_set)
-        .spawn();
-    }
-}
-
-#[allow(dead_code)]
-fn consume_bench(
-    q: &Arc<dyn OQueue<u64>>,
-    completed: &Arc<AtomicUsize>,
-    completed_wq: &Arc<ostd::sync::WaitQueue>,
-) {
-    println!("Populating queue");
-    for tid in 0..benchmark_consts::N_THREADS {
-        let mut cpu_set = ostd::cpu::set::CpuSet::new_empty();
-        cpu_set.add(ostd::cpu::CpuId::try_from(tid + 1).unwrap());
-        ThreadOptions::new({
-            let completed = completed.clone();
-            let completed_wq = completed_wq.clone();
-            let producer = q.attach_producer().unwrap();
-            move || {
-                for _ in 0..benchmark_consts::N_MESSAGES_PER_THREAD {
-                    producer.produce(0);
-                }
-                completed.fetch_add(1, Ordering::Relaxed);
-                completed_wq.wake_all();
-            }
-        })
-        .cpu_affinity(cpu_set)
-        .spawn();
-    }
-    completed_wq.wait_until(|| {
-        (completed.load(Ordering::Relaxed) == benchmark_consts::N_THREADS).then_some(())
-    });
-    completed.store(0, Ordering::Relaxed);
-
-    // Start all producers
-    for tid in 0..benchmark_consts::N_THREADS {
-        let mut cpu_set = ostd::cpu::set::CpuSet::new_empty();
-        cpu_set.add(ostd::cpu::CpuId::try_from(tid + 1).unwrap());
-        ThreadOptions::new({
-            let completed = completed.clone();
-            let completed_wq = completed_wq.clone();
-            let consumer = q.attach_consumer().unwrap();
-            move || {
-                let now = time::clocks::RealTimeClock::get().read_time();
-                for _ in 0..benchmark_consts::N_MESSAGES_PER_THREAD {
-                    let _ = consumer.consume();
-                }
-                let end = time::clocks::RealTimeClock::get().read_time();
-                println!(
-                    "[consumer-{}-{:?}] recv msg in {:?}",
-                    tid,
-                    ostd::cpu::CpuId::current_racy(),
-                    end - now
-                );
-                completed.fetch_add(1, Ordering::Relaxed);
-                completed_wq.wake_all();
-            }
-        })
-        .cpu_affinity(cpu_set)
-        .spawn();
-    }
-}
-
 fn init_thread() {
     println!("[kernel] Spawn init thread");
     // Work queue should be initialized before interrupt is enabled,
@@ -317,7 +172,7 @@ fn init_thread() {
 
     let q: Arc<dyn OQueue<u64>> = benchmark_consts::get_oq();
     // produce_bench(&q, &completed, &completed_wq);
-    consume_bench(&q, &completed, &completed_wq);
+    benchmark_consts::benchfn(&q, &completed, &completed_wq);
 
     println!("Waiting for benchmark to complete");
     // Exit after benchmark completes
