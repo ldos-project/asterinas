dc1e2052831dc512e293c8a5d7968dcb0c229154
Arthur Peters <amp@cs.utexas.edu>
2025-12-19 12:47:35 -0600

    Fast stack capture. (#132)
    Add support for quickly capturing the current stack into a fixed size buffer for later use. This is useful for capturing state to provide information for later potential errors.

---
+++ ostd/src/stacktrace.rs
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//! Utilities for quickly capturing and storing stack traces.
+//!
+//! This provides a way to capture a stack trace at interesting points and print it later if some
+//! error occurs. A good example is capturing the stack when a lock is acquired and then printing
+//! that stack of a deadlock is detected.
+//!
+//! This does nothing and consumes no space unless the `capture_stacks` feature is enabled.
+
+/// The number of stack frames to capture [`CapturedStackTrace`]. This defines the size of that data
+/// structure.
+pub const STACK_DEPTH: usize = 8;
+
+/// A compact stack trace representation with at most [`STACK_DEPTH`] frames.
+#[derive(Clone, Default)]
+pub struct CapturedStackTrace {
+    #[cfg(feature = "capture_stacks")]
+    frames: tinyvec::ArrayVec<[usize; STACK_DEPTH]>,
+}
+
+impl CapturedStackTrace {
+    /// Print the stack trace as a bare series of addresses.
+    pub fn print(&self) {
+        #[cfg(feature = "capture_stacks")]
+        {
+            use crate::early_println;
+            for frame in self.frames.iter() {
+                early_println!("0x{:x}", frame);
+            }
+        }
+    }
+
+    /// Capture the first [`STACK_DEPTH`] frames of the stack. This will skip over the first `skip`
+    /// frames counting from the caller to this function. So, with `skip = 0` the first captured
+    /// frame will be the caller to this function.
+    pub fn capture(skip: usize) -> Self {
+        #[cfg(feature = "capture_stacks")]
+        {
+            use core::ffi::c_void;
+
+            use unwinding::abi::{
+                _Unwind_Backtrace, _Unwind_GetIP, UnwindContext, UnwindReasonCode,
+            };
+
+            struct CallbackData {
+                count: usize,
+                skip: usize,
+                res: CapturedStackTrace,
+            }
+            extern "C" fn callback(
+                unwind_ctx: &UnwindContext<'_>,
+                arg: *mut c_void,
+            ) -> UnwindReasonCode {
+                let data = unsafe { &mut *(arg as *mut CallbackData) };
+                let pc = _Unwind_GetIP(unwind_ctx);
+                if pc == 0 {
+                    return UnwindReasonCode::NORMAL_STOP;
+                }
+                if data.count >= data.skip {
+                    if data.res.frames.try_push(pc).is_some() {
+                        // Stop if there is no more space available in the frames vec.
+                        return UnwindReasonCode::NORMAL_STOP;
+                    }
+                }
+                data.count += 1;
+                UnwindReasonCode::NO_REASON
+            }
+
+            let mut data = CallbackData {
+                skip: skip + 1,
+                count: 0,
+                res: Default::default(),
+            };
+            _Unwind_Backtrace(callback, &mut data as *mut _ as _);
+            data.res
+        }
+        #[cfg(not(feature = "capture_stacks"))]
+        {
+            let _ = skip;
+            Default::default()
+        }
+    }
+}
