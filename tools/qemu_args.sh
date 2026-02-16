4badb6474a2f413e4e860a2c819836ac1d399132
Yingqi Cao <yingqi@utexas.edu>
2026-02-15 22:38:18 -0600

    edited the mount points

--- tools/qemu_args.sh
+++ tools/qemu_args.sh
@@ -89,9 +89,9 @@ COMMON_QEMU_ARGS="\
     -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
     -drive if=none,format=raw,id=x0,file=./test/build/ext2.img \
     -drive if=none,format=raw,id=x1,file=./test/build/exfat.img \
-    -drive if=none,format=raw,id=r0,file=/dev/nvme0n1p1,cache=directsync \
-    -drive if=none,format=raw,id=r1,file=/dev/nvme1n1p1,cache=directsync \
-    -drive if=none,format=raw,id=r2,file=/dev/nvme2n1p1,cache=directsync \
+    -drive if=none,format=raw,id=r0,file=./mounts/nvme0n1p1/fs.img \
+    -drive if=none,format=raw,id=r1,file=./mounts/nvme1n1p1/fs.img \
+    -drive if=none,format=raw,id=r2,file=./mounts/nvme2n1p1/fs.img \
 "
 
 if [ "$1" = "iommu" ]; then
