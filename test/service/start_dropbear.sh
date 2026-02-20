81a10eaa37ef7decc7855d16d13bd092165c7142
Yingqi Cao <yingqi@utexas.edu>
2026-02-19 22:01:57 -0600

    added error handling

--- test/service/start_dropbear.sh
+++ test/service/start_dropbear.sh
@@ -3,9 +3,12 @@
 set -e
 
 # Script to start the dropbear ssh server
-/usr/sbin/dropbear -p 10.0.2.15:22 &
-
-echo "Dropbear started (PID $!)."
-lsof -p $!
+if [ -x /usr/sbin/dropbear ]; then
+	/usr/sbin/dropbear -p 10.0.2.15:22 &
+	echo "Dropbear started (PID $!)."
+	lsof -p $!
+else
+	echo "\033[31mWarning: dropbear not found, should not use run_dropbear!\033[0m"
+fi
 
 exec sh
