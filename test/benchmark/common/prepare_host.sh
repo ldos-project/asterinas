6ee716395f78c9402ef67ffd0da2d6374c19d16a
Aneesh Durg <aneeshdurg17@gmail.com>
2026-03-17 12:56:41 -0500

    Add a redis YCSB benchmark script (#163)

--- test/benchmark/common/prepare_host.sh
+++ test/benchmark/common/prepare_host.sh
@@ -45,3 +45,36 @@ prepare_fs() {
     mke2fs -F -O ^ext_attr -O ^resize_inode -O ^dir_index ${BENCHMARK_ROOT}/../build/ext2.img
     make initramfs BENCHMARK=${benchmark}
 }
+
+JDK_URL="https://download.oracle.com/java/25/latest/jdk-25_linux-x64_bin.tar.gz"
+MVN_URL="https://archive.apache.org/dist/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.tar.gz"
+export MVN_DIR=$(realpath ".cache/apache-maven-3.9.12")
+export YCSB_PATH=$(realpath ".cache/ycsb")
+export JDK_PATH=$(realpath ".cache/jdk-25.0.2")
+export JAVA_HOME=$JDK_PATH
+
+prepare_ycsb() {
+  mkdir -p .cache
+  (
+    cd .cache
+    if [ ! -d "$JDK_PATH" ]; then
+      wget "$JDK_URL" -O jdk.tar.gz
+      tar -xvf ./jdk.tar.gz
+    fi
+
+    if [ ! -d "$MVN_DIR" ]; then
+      wget $MVN_URL
+      tar -xvf ./apache-maven-3.9.12-bin.tar.gz
+    fi
+
+    if [ ! -d "$YCSB_PATH" ]; then
+      git clone https://github.com/brianfrankcooper/YCSB --depth=1 $YCSB_PATH
+
+      # Build
+      pushd $YCSB_PATH
+      $MVN_DIR/bin/mvn -pl site.ycsb:redis-binding -am clean package
+      $MVN_DIR/bin/mvn -pl site.ycsb:memcached-binding -am clean package
+      popd
+    fi
+  )
+}
---
+++ test/benchmark/redis/ycsb/host.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MPL-2.0
+
+set -e
+
+# Function to stop the guest VM
+stop_guest() {
+    echo "Stopping guest VM..."
+    # `-r` means if there's no qemu, the kill won't be executed
+    pgrep qemu | xargs -r kill
+}
+
+# Trap EXIT signal to ensure guest VM is stopped on script exit
+trap stop_guest EXIT
+
+# TODO - export some variables like MVN_PATH/YCSB_DIR from prepare_host.sh
+export PATH=$PATH:$(realpath .cache/apache-maven-3.9.12/bin/)
+
+# Run YCSB + redis bench
+echo "Running YCSB bench connected to $GUEST_SERVER_IP_ADDRESS"
+export JAVA_HOME=$(realpath ".cache/jdk-25.0.2")
+
+cd $YCSB_PATH/
+./bin/ycsb load redis -p redis.host="$GUEST_SERVER_IP_ADDRESS" -p redis.port="6379" -P ./workloads/workloada
+./bin/ycsb run redis -p redis.host="$GUEST_SERVER_IP_ADDRESS" -p redis.port="6379" -P ./workloads/workloada
+
+# The trap will automatically stop the guest VM when the script exits
