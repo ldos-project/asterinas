342a4a6a5ac4a49550a1f77375c56d8bccd16823
Aneesh Durg <aneeshdurg17@gmail.com>
2026-03-25 15:41:53 +0000

    parse_datacapture error messages

--- tools/parse_datacapture.py
+++ tools/parse_datacapture.py
@@ -17,14 +17,14 @@ class DtlbMisses:
     def read_record(cls, f: IO[bytes]) -> "DtlbMisses":
         timestamp = int.from_bytes(f.read(16), ENDIANESS)
         if timestamp == 0:
-            raise EOFError
+            raise EOFError(f"{f.seek(0, 1)}")
         miss_l1_tlb = int.from_bytes(f.read(8), ENDIANESS)
         miss_all_tlb = int.from_bytes(f.read(8), ENDIANESS)
         return DtlbMisses(timestamp, miss_l1_tlb, miss_all_tlb)
 
 
 with open(sys.argv[1], "rb") as f:
-    assert f.read(len(MAGIC)) == MAGIC
+    assert (b := f.read(len(MAGIC))) == MAGIC, b
 
     json_bytes = b""
     while (b := f.read(1)) != b"\x00":
