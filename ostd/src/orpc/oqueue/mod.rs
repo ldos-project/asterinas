c1267b633fc4698d8a44832618ae0147d3fedcd1
Yingqi Cao <yingqi@utexas.edu>
2026-04-10 23:33:48 +0000

    IMPORTANT: fix oqueue panics by preventing the cursor underflow using wrapping_sub.

--- ostd/src/orpc/oqueue/mod.rs
+++ ostd/src/orpc/oqueue/mod.rs
@@ -618,7 +618,7 @@ impl Sub<usize> for Cursor {
     type Output = Cursor;
 
     fn sub(self, rhs: usize) -> Self::Output {
-        Cursor(self.0 - rhs)
+        Cursor(self.0.wrapping_sub(rhs))
     }
 }
 
