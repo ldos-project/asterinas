07204f543eb78f99979e2fe24997771b2f40d9b2
Arthur Peters <amp@singingwizard.org>
2026-04-21 11:58:30 -0500

    Improve sync'ing

--- kernel/src/data_capture.rs
+++ kernel/src/data_capture.rs
@@ -3,11 +3,47 @@
 //! OQueue data capture utilities.
 
 use alloc::{boxed::Box, sync::Arc, vec::Vec};
+use core::time::Duration;
 
-use log::info;
-use ostd::{ignore_err, sync::Mutex};
+use log::{error, info};
+use ostd::{
+    ignore_err, new_server,
+    orpc::{
+        framework::{notifier::Notifier as _, spawn_thread},
+        orpc_server,
+    },
+    sync::Mutex,
+};
 
-use crate::fs;
+use crate::{fs, kcmdline, util::timer::TimerServer};
+
+#[orpc_server]
+struct DataCaptureManager {}
+
+impl DataCaptureManager {
+    pub fn spawn(period: Duration) -> Arc<Self> {
+        let server = new_server!(|_| Self {});
+        spawn_thread(server.clone(), {
+            let server = server.clone();
+            move || server.main(period)
+        });
+        server
+    }
+
+    pub fn main(&self, period: Duration) -> Result<(), Box<dyn core::error::Error>> {
+        let notify_server = TimerServer::spawn(period);
+
+        let notify_observer = notify_server
+            .notification_oqueue()
+            .attach_strong_observer()?;
+        loop {
+            notify_observer.strong_observe();
+            for f in DATA_CAPTURE_FILE_SYNCERS.lock().iter() {
+                f();
+            }
+        }
+    }
+}
 
 static DATA_CAPTURE_DEVICE_LEGACY: Mutex<
     Option<Arc<dyn mariposa_data_capture::legacy::DataCaptureDevice>>,
@@ -19,21 +55,43 @@ static DATA_CAPTURE_DEVICE: Mutex<Option<Arc<dyn mariposa_data_capture::DataCapt
 pub(super) static DATA_CAPTURE_FILE_FINALIZERS: Mutex<Vec<Box<dyn Fn() + Send>>> =
     Mutex::new(Vec::new());
 
+static DATA_CAPTURE_FILE_SYNCERS: Mutex<Vec<Box<dyn Fn() + Send>>> = Mutex::new(Vec::new());
+
 pub(super) fn start_capture_devices() {
-    if let Ok(capture_block_device) = fs::start_block_device("capture_legacy") {
-        DATA_CAPTURE_DEVICE_LEGACY.lock().replace(
-            mariposa_data_capture::legacy::DataCaptureDeviceServer::new(capture_block_device),
-        );
-        info!("[kernel] Initialized legacy data capture device (capture_legacy)");
+    match fs::start_block_device("capture_legacy") {
+        Ok(capture_block_device) => {
+            DATA_CAPTURE_DEVICE_LEGACY.lock().replace(
+                mariposa_data_capture::legacy::DataCaptureDeviceServer::new(capture_block_device),
+            );
+            info!("[kernel] Initialized legacy data capture device (capture_legacy)");
+        }
+        Err(e) => error!(
+            "[kernel] Failed to initialize legacy data capture device (capture_legacy): {}",
+            e
+        ),
     }
 
-    if let Ok(capture_block_device) = fs::start_block_device("capture") {
-        DATA_CAPTURE_DEVICE
-            .lock()
-            .replace(mariposa_data_capture::DataCaptureDeviceServer::new(
-                capture_block_device,
-            ));
-        info!("[kernel] Initialized new data capture device (capture)");
+    match fs::start_block_device("capture") {
+        Ok(capture_block_device) => {
+            DATA_CAPTURE_DEVICE.lock().replace(
+                mariposa_data_capture::DataCaptureDeviceServer::new(capture_block_device),
+            );
+            info!("[kernel] Initialized new data capture device (capture)");
+        }
+        Err(e) => error!(
+            "[kernel] Failed to initialized new data capture device (capture): {}",
+            e
+        ),
+    }
+
+    // Start a server which syncs the data_capture devices every `secs` seconds based on the
+    // kcmdline arg `data_capture.sync_period`. If `data_capture.sync_period` is not provided or is
+    // <= 0, then do not sync periodically.
+    if let Some(secs) = kcmdline::get_kernel_cmd_line()
+        .and_then(|cl| cl.get_module_arg_by_name("data_capture", "sync_period"))
+        && secs > 0.0
+    {
+        DataCaptureManager::spawn(Duration::from_secs_f32(secs));
     }
 }
 
@@ -41,8 +99,9 @@ pub(super) fn start_capture_devices() {
 pub fn new_legacy_data_capture_file<T: serde::Serialize + Copy + Send + 'static>(
     descriptor: mariposa_data_capture::legacy::FileDescriptor,
 ) -> Arc<dyn mariposa_data_capture::legacy::DataCaptureFile<T>> {
-    let data_capture_device = DATA_CAPTURE_DEVICE_LEGACY.lock().clone();
-    let ret = data_capture_device
+    let ret = DATA_CAPTURE_DEVICE_LEGACY
+        .lock()
+        .as_ref()
         .unwrap()
         .new_file(descriptor)
         .unwrap()
@@ -51,6 +110,13 @@ pub fn new_legacy_data_capture_file<T: serde::Serialize + Copy + Send + 'static>
         let ret = ret.clone();
         move || {
             ignore_err!(ret.stop());
+            info!("[kernel] Stopped data capture device (capture)");
+        }
+    }));
+    DATA_CAPTURE_FILE_SYNCERS.lock().push(Box::new({
+        let ret = ret.clone();
+        move || {
+            ignore_err!(ret.sync());
             info!("[kernel] Sync'd data capture device (capture)");
         }
     }));
@@ -61,8 +127,9 @@ pub fn new_legacy_data_capture_file<T: serde::Serialize + Copy + Send + 'static>
 pub fn new_data_capture_file<T: serde::Serialize + Copy + Send + 'static>(
     descriptor: mariposa_data_capture::FileDescriptor,
 ) -> Arc<dyn mariposa_data_capture::DataCaptureFile<T>> {
-    let data_capture_device = DATA_CAPTURE_DEVICE.lock().clone();
-    let ret = data_capture_device
+    let ret = DATA_CAPTURE_DEVICE
+        .lock()
+        .as_ref()
         .unwrap()
         .new_file(descriptor)
         .unwrap()
@@ -71,6 +138,13 @@ pub fn new_data_capture_file<T: serde::Serialize + Copy + Send + 'static>(
         let ret = ret.clone();
         move || {
             ignore_err!(ret.stop());
+            info!("[kernel] Stopped legacy data capture device (capture_legacy)");
+        }
+    }));
+    DATA_CAPTURE_FILE_SYNCERS.lock().push(Box::new({
+        let ret = ret.clone();
+        move || {
+            ignore_err!(ret.sync());
             info!("[kernel] Sync'd legacy data capture device (capture_legacy)");
         }
     }));
