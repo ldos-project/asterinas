dcf8e65e43f8bf501d9dddfe07af4a3e766de894
Arthur Peters <amp@cs.utexas.edu>
2026-05-21 15:35:57 -0500

    Add impls for ThreadName (#219)
    This adds Display and Clone. This makes various passing around
    and debugging easier.

--- kernel/src/process/posix_thread/name.rs
+++ kernel/src/process/posix_thread/name.rs
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: MPL-2.0
 
+use core::fmt::{Display, Formatter};
+
 use crate::prelude::*;
 
 pub const MAX_THREAD_NAME_LEN: usize = 16;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ThreadName {
     inner: [u8; MAX_THREAD_NAME_LEN],
     count: usize,
+}
+
+impl Display for ThreadName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        f.write_str(
+            &self
+                .name()
+                .unwrap_or_default()
+                .map(|v| v.to_string_lossy())
+                .unwrap_or("[unnamed]".into()),
+        )
+    }
 }
 
 impl Default for ThreadName {
@@ -52,5 +66,42 @@ impl ThreadName {
 
     pub fn name(&self) -> Result<Option<&CStr>> {
         Ok(Some(CStr::from_bytes_until_nul(&self.inner)?))
+    }
+}
+
+#[cfg(ktest)]
+mod test {
+    use alloc::format;
+
+    use ostd::prelude::*;
+
+    use super::*;
+
+    #[ktest]
+    fn display_shows_name() {
+        let mut name = ThreadName::new();
+        name.set_name(c"hello").unwrap();
+        assert_eq!(format!("{}", name), "hello");
+
+        name.set_name(c"hel\xFFo").unwrap();
+        assert_eq!(format!("{}", name), "hel\u{FFFD}o");
+    }
+
+    #[ktest]
+    fn set_name_truncates_when_too_long() {
+        // 16 chars + nul = 17 bytes > MAX_THREAD_NAME_LEN
+        let mut name = ThreadName::new();
+        let cname = CString::new("1234567890123456").unwrap();
+        name.set_name(&cname).unwrap();
+        assert_eq!(name.name().unwrap().unwrap().to_bytes(), b"123456789012345");
+    }
+
+    #[ktest]
+    fn from_executable_path_extracts_filename() {
+        let name = ThreadName::new_from_executable_path("/usr/bin/myapp").unwrap();
+        assert_eq!(name.name().unwrap().unwrap().to_bytes(), b"myapp");
+
+        let name = ThreadName::new_from_executable_path("myapp").unwrap();
+        assert_eq!(name.name().unwrap().unwrap().to_bytes(), b"myapp");
     }
 }
