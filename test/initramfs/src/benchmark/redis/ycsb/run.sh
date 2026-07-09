2b795592c09e5193a1470e0d2a1dec3c4d4a1c3f
gvipat <gaurav.vipat@gmail.com>
2026-07-09 02:00:20 -0600

    Move YCSB config to test config directory.

--- test/initramfs/src/benchmark/redis/ycsb/run.sh
+++ test/initramfs/src/benchmark/redis/ycsb/run.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 echo "Running redis server"
-/benchmark/bin/redis-server /benchmark/redis/ycsb/ycsb.conf &
+/benchmark/bin/redis-server /etc/ycsb.conf &
 echo "waiting for stop message"
 /bin/nc -l -s 10.0.2.15 -p 5201 -e true
 echo "done"
