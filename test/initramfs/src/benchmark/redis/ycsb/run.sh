ca695cd2553dbe48299de2799f7186fe19f9170f
gvipat <gaurav.vipat@gmail.com>
2026-07-09 01:29:59 -0600

    Update redis-server invocation to nix install path.

--- test/initramfs/src/benchmark/redis/ycsb/run.sh
+++ test/initramfs/src/benchmark/redis/ycsb/run.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 echo "Running redis server"
-/usr/local/redis/bin/redis-server /benchmark/redis/ycsb/ycsb.conf &
+/benchmark/bin/redis-server /benchmark/redis/ycsb/ycsb.conf &
 echo "waiting for stop message"
 /bin/nc -l -s 10.0.2.15 -p 5201 -e true
 echo "done"
