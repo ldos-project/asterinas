70624ab13d0ed88f168d3b7f72e59bffd12632fe
Mitchell Fanger <mfanger@mitchells-mbp.lan>
2026-07-20 10:30:54 -0400

    update devcontainer.template.json

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -123,9 +123,8 @@ update_docker_image_version() {
 update_all_docker_version_refs() {
     new_version=$(cat ${DOCKER_IMAGE_VERSION_PATH})
 
-    # Update Docker image versions in devcontainer.json, README files, and AGENTS.md
-    # why does devcontainer.json not exist
-    #update_image_versions ${ASTER_SRC_DIR}/.devcontainer/devcontainer.json
+    # Update Docker image versions in devcontainer.template.json, README files, and AGENTS.md
+    update_image_versions ${ASTER_SRC_DIR}/.devcontainer/devcontainer.template.json
     update_image_versions ${ASTER_SRC_DIR}/README.md
     update_image_versions ${ASTER_SRC_DIR}/AGENTS.md
     update_image_versions ${SCRIPT_DIR}/docker/README.md
