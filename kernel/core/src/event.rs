e77457e597c422bf2207836e18c2dbbed5ce2d4b
Arthur Peters <amp@cs.utexas.edu>
2026-09-15 17:42:13 +0000

    Add location to EventContext serialization

--- kernel/core/src/event.rs
+++ kernel/core/src/event.rs
@@ -1,24 +1,71 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use aster_time::Instant;
+use aster_util::fixed_str::FixedCStr;
 use ostd::task::Task;
-use serde::Serialize;
+use ser::SerializeTupleVariant;
+use serde::{
+    Serialize, Serializer,
+    ser::{self, SerializeTupleStruct},
+};
+use snafu::Location;
 
 use crate::{process::posix_thread::AsPosixThread as _, thread::Tid};
 
-#[derive(Debug, Clone, Copy, Serialize)]
+#[derive(Clone, Copy, Debug)]
+pub struct SerializableLocation(Location);
+
+impl Serialize for SerializableLocation {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut tuple = serializer.serialize_tuple_struct("Location", 3)?;
+        tuple.serialize_field(self.0.file)?;
+        tuple.serialize_field(&self.0.line)?;
+        tuple.serialize_field(&self.0.column)?;
+        tuple.end()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum TaskId {
-    KernelTask(usize),
-    PosixThread(Tid),
+    KernelTask(usize, Location),
+    PosixThread(Tid, FixedCStr<16>),
     Unknown,
 }
 
 impl TaskId {
     pub fn new(task: &Task) -> Self {
         if let Some(t) = task.as_posix_thread() {
-            Self::PosixThread(t.tid())
+            Self::PosixThread(t.tid(), *t.thread_name().lock())
         } else {
-            Self::KernelTask(task.id().into())
+            Self::KernelTask(task.id().into(), task.build_location())
+        }
+    }
+}
+
+impl Serialize for TaskId {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            TaskId::KernelTask(id, location) => {
+                // Serializer::serialize_tuple(self, len)
+                let mut serde_state =
+                    serializer.serialize_tuple_variant("TaskId", 0u32, "KernelTask", 2)?;
+                serde_state.serialize_field(id)?;
+                serde_state.serialize_field(&SerializableLocation(*location))?;
+                serde_state.end()
+            }
+            TaskId::PosixThread(id, name) => {
+                let mut serde_state =
+                    serializer.serialize_tuple_variant("TaskId", 1u32, "PosixThread", 2)?;
+                serde_state.serialize_field(id)?;
+                serde_state.serialize_field(name)?;
+                serde_state.end()
+            }
+            TaskId::Unknown => {
+                Serializer::serialize_unit_variant(serializer, "TaskId", 2u32, "Unknown")
+            }
         }
     }
 }
